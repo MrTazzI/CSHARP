ae11ee5cd228823938988a468cdeab31b297844d
MrTazzI <154403699+MrTazzI@users.noreply.github.com>
2024-02-22 14:12:24 +0500

    Add Lesson2: Task2

--- Lesson2/Task2/Program.cs
+++ Lesson2/Task2/Program.cs
@@ -1,1 +1,13 @@
-﻿// 
+﻿int n = 12;
+int[] arr = new int[n];
+int i = 0;
+
+// while( i < arr.Length)
+while (i < n)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
