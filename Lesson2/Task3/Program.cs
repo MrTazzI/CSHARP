06e7c06a806a8a262c805a98ee6455ecd77136ce
MrTazzI <154403699+MrTazzI@users.noreply.github.com>
2024-02-22 14:45:33 +0500

    add Lesson2: Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// int n = 10;
+int[] array = { 1, 5, 4, 2, 9, 6, 1, 3, 9, 5 };
+int i = 0;
+
+// while (i < n) можно и так но тогда надо убрать массив 1 строка
+while (i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1; 
+}
