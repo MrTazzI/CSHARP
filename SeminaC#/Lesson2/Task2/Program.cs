c905bb5da5f5cca741a8cd8fc5ec93311d845c43
MrTazzI <154403699+MrTazzI@users.noreply.github.com>
2024-02-26 15:56:42 +0500

    Add lesson2

---
+++ SeminaC#/Lesson2/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Наришите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int result = num/100*10 +  num%10;
+Console.WriteLine(result);
---
+++ SeminaC#/Lesson2/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, котора принимает на 
+// вход два числа и проверяет, является ли 
+// одно число квадратом другого.
+
+// 5, 25 ->  да 
+// -4, 16 ->  да 
+// 26, 55 -> да 
+// 9, 9 -> нет
+
+Console.WriteLine("Ввидите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввидите число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2*num2 || num2 == num1*num1)
+{
+    System.Console.WriteLine("Да");
+}
+else 
+{
+    System.Console.WriteLine("Нет");
+}
